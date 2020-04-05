43a96e50110db024c8a4719e60974989a45ef689
Evan Savage <evan@savageinter.net>
2020-04-05 15:59:24 -0400

    Base layout, robots.txt, start of i18n stuff
    The idea here is to have separate folders `/en`, `/es`, `/de`, etc. for
    different languages, and to translate these pages into each folder as we
    go along.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
-module.exports = {
-  dir: {
-    input: "src",
-  },
-  templateFormats: [
-    'html',
-    'jpg',
-    'png',
-  ],
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+    },
+    htmlTemplateEngine: 'njk',
+    templateFormats: [
+      'css',
+      'html',
+      'ico',
+      'jpg',
+      'js',
+      'njk',
+      'png',
+      'txt',
+    ],
+  };
 };
