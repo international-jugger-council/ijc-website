8d9577de14b3a2fbaf28c32a852948e888319efe
Evan Savage <evan@savageinter.net>
2020-04-05 17:09:37 -0400

    Add navbar mockup
    Doesn't do anything yet, but will eventually.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/bulma/css/bulma.min.css': 'css/bulma.min.css',
+  });
   return {
     dir: {
       input: 'src',
